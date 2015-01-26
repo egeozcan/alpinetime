5a27367a9525242d16917f49e22c9870e4e5bbcf
ege <ege@localhost>
2015-01-26 22:37:37 +0100

    still thinking about how to structure

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -1,5 +1,40 @@
 var React = require('react');
 
+var projects = [
+  {
+    Name: "Test1",
+    Start: new Date(),
+    Deadline: new Date(),
+    Packages: [
+      {
+        Name: "Package1"
+        Start: new Date(),
+        Category: "Cat1",
+        Tasks: [
+          {
+            Description: "do stuff",
+            Category: "TaskCat1",
+            Estimation: 8,
+            AssignedTo: "ege.ozcan",
+            EstimatedBy: "ege.ozcan",
+            Tags: ["testTag", "testTag2"],
+            Status: "Released"
+          },
+          {
+            Description: "do other stuff",
+            Category: "TaskCat2",
+            Estimation: 8,
+            AssignedTo: "ege.ozcan",
+            EstimatedBy: "ege.ozcan",
+            Tags: ["testTag", "testTag2"],
+            Status: "Released"
+          }
+        ]
+      }
+    ]
+  }
+]
+
 var HelloMessage = React.createClass({
   render() {
   	var styles = {
@@ -13,7 +48,5 @@ var HelloMessage = React.createClass({
   }
 });
 
-setInterval(function () {
-	var d = new Date();
-	React.render(<HelloMessage color="blue" name={(+d).toString()} />, document.getElementById("main"));
-}, 10);
+var d = new Date();
+React.render(<HelloMessage color="blue" name={(+d).toString()} />, document.getElementById("main"));
