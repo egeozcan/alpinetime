ce189959c1e6fd098abc99c62c03e32643ba370e
Yavuz Ege Özcan <185294+egeozcan@users.noreply.github.com>
2015-01-26 22:37:37 +0100

    still thinking about how to structure

---
+++ src/js/AlpineTimeControl.js
@@ -0,0 +1,19 @@
+var AlpineTimeControl = {
+  _stores: [],
+  addStore: function(store) {
+    this._stores.push(store)
+  },
+  trigger: function() {
+    var args = [].slice.call(arguments)
+    this._stores.forEach(function(el,i,a){
+      el.trigger.apply(null, args)
+    })
+  },
+  on: function(ev, callback) {
+    this._stores.forEach(function(el,i,a){
+      el.on(ev, callback)
+    })
+  }
+}
+
+module.exports = AlpineTimeControl;
