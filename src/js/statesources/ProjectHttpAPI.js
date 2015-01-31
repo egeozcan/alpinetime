3dc71d600ec51f5328a058eb0509286ac893486b
Yavuz Ege Özcan <185294+egeozcan@users.noreply.github.com>
2015-01-31 22:08:33 +0100

    was thinking about mocking an api but changed mind, commit for future self depreciating humor

---
+++ src/js/statesources/ProjectHttpAPI.js
@@ -0,0 +1,10 @@
+export default Marty.createStateSource({
+  getProject: function (id) {
+    var reject, resolve;
+    return (new Promise(function () {
+      [reject, resolve] = arguments;
+    })).then(function () {
+
+    });
+  }
+});
