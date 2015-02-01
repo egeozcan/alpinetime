79d270c7f530aecabf458b57d524b095d6fd096c
ege ozcan <egecan@gmail.com>
2015-02-01 14:47:24 +0100

    some restructuring with koa and reflux

--- src/js/actioncreators/ProjectSourceActionCreator.js
+++
@@ -1,6 +0,0 @@
-var Marty = require('marty');
-var ProjectConstants = require('../constants/ProjectConstants');
-
-var ProjectSourceActionCreator = Marty.createActionCreators({
-    receiveProject: ProjectConstants.PROJECT_RECEIVE
-});
