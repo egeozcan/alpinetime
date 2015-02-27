d44690dc1617a82b534b5276e5688e63534817d9
Yavuz Ege Ozcan <egecan@gmail.com>
2015-02-27 14:27:33 +0100

    fixed install

--- install.js
+++ install.js
@@ -1,10 +1,11 @@
 var cp = require('child_process');
 var commands = [
-  "npm install -g gulp-cli",
+	"mkdir public", //todo: replace with module mkdirp
   "npm install",
-  "go get",
   "go get github.com/jteeuwen/go-bindata/...",
-  "go get github.com/elazarl/go-bindata-assetfs/..."
+  "go get github.com/elazarl/go-bindata-assetfs/...",
+	"go-bindata -pkg data -o ./data/bindata.go ./public/...",
+  "go get"
 ];
 
 commands.forEach(function (command) {
