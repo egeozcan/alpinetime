5c733b83f8edbc8b21919484d9d83d90d157f64f
Yavuz Ege Özcan <185294+egeozcan@users.noreply.github.com>
2015-02-27 19:36:43 +0100

    fixed asset handling

--- middleware/checkasset.go
+++ middleware/checkasset.go
@@ -3,6 +3,8 @@ package middleware
 import (
 	"alpinetime/data"
 	"github.com/gin-gonic/gin"
+	"mime"
+	"path/filepath"
 	"strings"
 )
 
@@ -10,9 +12,22 @@ func CheckAsset(c *gin.Context) {
 	if !strings.HasPrefix(c.Request.URL.Path, "/public") {
 		return
 	}
-	asset, err := data.Asset(strings.TrimPrefix(c.Request.URL.Path, "/"))
-	if err == nil {
-		c.Writer.Write(asset)
-		c.AbortWithStatus(200)
+	//get rid of the slash at the beginning of the path
+	fileName := c.Request.URL.Path[1:]
+	fileData, err := data.Asset(fileName)
+	if err != nil {
+		//no static file for this path is registered
+		return
 	}
+	ext := filepath.Ext(fileName)
+	var mimeType string
+	if ext != "" {
+		mimeType = mime.TypeByExtension(ext)
+	}
+	if mimeType == "" {
+		mimeType = "application/octet-stream"
+	}
+	c.AbortWithStatus(200)
+	c.Writer.Header().Set(`Content-Type`, mimeType)
+	c.Writer.Write(fileData)
 }
