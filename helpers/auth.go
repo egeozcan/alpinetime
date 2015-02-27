d2271f09e55cbbc24d95f4fbc1fd4962ff4017da
ege ozcan <egecan@gmail.com>
2015-02-27 19:36:43 +0100

    fixed asset handling

--- helpers/auth.go
+++ helpers/auth.go
@@ -1,21 +1,22 @@
 package helpers
 
 import (
-    "fmt"
-    "github.com/go-ldap/ldap"
+	"errors"
+	"fmt"
+	"github.com/go-ldap/ldap"
 )
 
 var ldap_server string = "l-mobile.intern"
 var ldap_port uint16 = 389
 
 func Auth(username, password string) error {
-    if(true) {
-		return nil
+	if true {
+		return errors.New("nope")
 	}
 	l, err := ldap.Dial("tcp", fmt.Sprintf("%s:%d", ldap_server, ldap_port))
-    if err != nil {
-        return err
-    }
-    defer l.Close()
-    return l.Bind(username, password)
+	if err != nil {
+		return err
+	}
+	defer l.Close()
+	return l.Bind(username, password)
 }
