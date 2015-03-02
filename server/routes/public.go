abdfe151e6b49a3de4464ee9725bada9dab019e8
ege ozcan <egecan@gmail.com>
2015-03-02 20:47:26 +0100

    why wasn't this included?!

---
+++ server/routes/public.go
@@ -0,0 +1,25 @@
+package routes
+
+import (
+	"alpinetime/data"
+	"alpinetime/helpers"
+	"fmt"
+	"github.com/gin-gonic/contrib/sessions"
+	"github.com/gin-gonic/gin"
+)
+
+func LoginPage(c *gin.Context) {
+	session := sessions.Default(c)
+	user := session.Get("username")
+	if user != nil && user != "" {
+		c.Redirect(301, "/list")
+		return
+	}
+	helpers.WriteHeader(c.Writer, 200, "text/html")
+	fileData, err := data.Asset("public/login.html")
+	if err != nil {
+		fmt.Errorf("error: %s", err)
+		return
+	}
+	c.Writer.Write(fileData)
+}
