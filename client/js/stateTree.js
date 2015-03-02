5e3b234cec7e5f309dec06361f4a3fec26046951
Yavuz Ege Ozcan <egecan@gmail.com>
2015-03-02 16:14:53 +0100

    not just hello world

--- client/js/stateTree.js
+++ client/js/stateTree.js
@@ -1,7 +1,27 @@
 var Baobab = require('baobab');
 export default new Baobab({
   stores: {
-    projects: [],
+    projects: [{
+      ID: 1,
+      CreatedAt: new Date(),
+      UpdatedAt: new Date(),
+      DeletedAt: null,
+      Name: "Test project 1",
+      Customer: {
+        Name: "Test customer 1",
+        LegacyId: "1112"
+      }
+    }, {
+      ID: 2,
+      CreatedAt: new Date(),
+      UpdatedAt: new Date(),
+      DeletedAt: null,
+      Name: "Test project 2",
+      Customer: {
+        Name: "Test customer 1",
+        LegacyId: "1112"
+      }
+    }],
     users: []
   },
   //this is the app state, never the component state
