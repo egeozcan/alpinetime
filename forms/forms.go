b5a4bdf8f6140e986bd7faf6332c62ee4b1d42d2
Yavuz Ege Özcan <185294+egeozcan@users.noreply.github.com>
2015-03-09 17:52:54 +0100

    empty stuff

--- forms/forms.go
+++ forms/forms.go
@@ -1,6 +1,22 @@
 package forms
 
+import (
+	"time"
+)
+
 type LoginForm struct {
-    User     string `form:"username" binding:"required"`
-    Password string `form:"password" binding:"required"`
+	User     string `form:"username" binding:"required"`
+	Password string `form:"password" binding:"required"`
+}
+
+type ProjectQuery struct {
+	Name        string `form:"name"`
+	Description string `form:"description"`
+	StartDate   time.Time
+}
+
+type PackageQuery struct {
+	Name        string `form:"name"`
+	Description string `form:"description"`
+	StartDate   time.Time
 }
