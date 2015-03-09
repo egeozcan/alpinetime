f496f4293f6d5d2a364f07fa750cbdf7df53a95c
Yavuz Ege Ozcan <egecan@gmail.com>
2015-03-09 17:09:48 +0100

    set log mode on in the debug file

--- server/routes/project.go
+++ server/routes/project.go
@@ -11,7 +11,6 @@ func Projects(c *gin.Context) {
 		ProtectedArea(c)
 		return
 	}
-	models.Db.LogMode(true)
 	dbResults := models.Db.Limit(10).Find(&[]models.Project{})
 	fmt.Printf("Affected: %v Rows", dbResults.RowsAffected)
 	c.JSON(200, dbResults.Value)
