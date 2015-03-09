c9bbba826eb3ecb5a742e2bf03da4c736294737b
Yavuz Ege Ozcan <egecan@gmail.com>
2015-03-09 16:48:57 +0100

    the app actually displays stuff from database on the screen!!1

--- client/js/stateTree.js
+++ client/js/stateTree.js
@@ -1,44 +1,7 @@
 var Baobab = require('baobab');
 export default new Baobab({
   stores: {
-    projects: [
-      {
-        ID: 1,
-        CreatedAt: new Date(),
-        UpdatedAt: new Date(),
-        DeletedAt: null,
-        Name: "Test project 1",
-        Description: "wow descrirptio... error",
-        Customer: {
-          Name: "Test customer 1",
-          LegacyId: "1112"
-        }
-      },
-      {
-        ID: 3,
-        CreatedAt: new Date(),
-        UpdatedAt: new Date(),
-        DeletedAt: null,
-        Name: "Test project 3",
-        Description: "333333!!!!1111",
-        Customer: {
-          Name: "Test customer 3",
-          LegacyId: "1112"
-        }
-      },
-      {
-        ID: 2,
-        CreatedAt: new Date(),
-        UpdatedAt: new Date(),
-        DeletedAt: null,
-        Name: "Test project 2",
-        Description: "Project 2 description",
-        Customer: {
-          Name: "Test customer 1",
-          LegacyId: "1112"
-        }
-      }
-    ],
+    projects: [],
     users: []
   },
   //this is the app state, never the component state
