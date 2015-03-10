bfda7e6c699acb59d64480d680c629417b75b7cf
ege ozcan <egecan@gmail.com>
2015-03-11 00:16:47 +0100

    trying to get the hang of react

--- client/js/actions/projectActions.js
+++ client/js/actions/projectActions.js
@@ -1,12 +1,43 @@
 var request = require("superagent"); 
 var state = require("../stateTree.js");
+var projectStore = state.select("stores", "projects");
+
+function loadProject(id) {
+  id = id.toString();
+  return projectStore.select(p => p.ID === id);
+}
 
 export default {
-  add: function (project) {
+  add(project) {
     // var id = project.ID;
     // var availableIds = stateTree
     //   .select("models", "projects")
     //   .map(p => p.ID);
     
+  },
+  load(id) {
+    var project = loadProject(id).get();
+    if (project !== undefined) {
+      return;
+    }
+    projectStore.push({
+      ID: id,
+      _isLoading: true
+    });
+    request
+      .get("/app/project/" + id)
+      .end(function (err, res) {
+        if (!!err) {
+          loadProject(id).merge({
+            _isLoading: false,
+            _invalid: true,
+            _error: err
+          });
+          return;
+        }
+        loadProject(id)
+          .merge(res)
+          .set(_isLoading: false);
+      });
   }
 }
