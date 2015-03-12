059dbecb9d4de3276b265f4693f785b1704a3a83
Yavuz Ege Özcan <185294+egeozcan@users.noreply.github.com>
2015-03-12 16:39:35 +0100

    project details thing

--- client/js/components/project/Project.jsx
+++ client/js/components/project/Project.jsx
@@ -1,7 +1,15 @@
 var projectActions = require("../../actions/projectActions.js");
 var ReactPivot = require('react-pivot');
+var Router = require('react-router');
 
 var Project = React.createClass({
+    mixins: [Router.Navigation, Router.State],
+    componentWillMount() {
+        projectActions.load(this.getParams().ID);
+    },
+    componentWillReceiveProps() {
+        projectActions.load(this.getParams().ID);
+    },
     render() {
         var project = this.props.project;
         var tasks;
@@ -17,6 +25,9 @@ var Project = React.createClass({
                 <hr />
                 <strong>For customer:</strong>
                 <span>{project.Customer.Name}</span>
+                <hr />
+                <h3>Tasks</h3>
+                {tasks}
             </div>
         );
     }
