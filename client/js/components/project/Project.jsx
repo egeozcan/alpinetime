afe9a38873c5fd7e0b2dd304f553d5bb177f411d
ege ozcan <egecan@gmail.com>
2015-03-12 22:38:32 +0100

    actually displays a project

--- client/js/components/project/Project.jsx
+++ client/js/components/project/Project.jsx
@@ -1,9 +1,12 @@
 var projectActions = require("../../actions/projectActions.js");
 var ReactPivot = require('react-pivot');
 var Router = require('react-router');
+var Tree = require("../../stateTree.js");
+var React = require('react');
 
-var Project = React.createClass({
-    mixins: [Router.Navigation, Router.State],
+export default React.createClass({
+    mixins: [Router.Navigation, Router.State, Tree.mixin],
+    cursors: { projects: ['stores', 'projects'] },
     componentWillMount() {
         projectActions.load(this.getParams().ID);
     },
@@ -11,20 +14,24 @@ var Project = React.createClass({
         projectActions.load(this.getParams().ID);
     },
     render() {
-        var project = this.props.project;
-        var tasks;
-        var dimensions = {
+        console.log(this.cursors.projects.get());
+        var project = this.cursors.projects.select(p => p.ID === this.getParams().ID).get();
+        if (!project) {
+            return (<span>Loading...</span>);
+        };
+        var tasks = "";
+        /*var dimensions = {
             {value:"Package.Name", title: "Package"}
         };
         if (project.Tasks.length > 0) {
-            tasks = (<ReactPivot rows={project.Tasks} dimensions={dimensions} />
-        };
+            tasks = "" (<ReactPivot rows={project.Tasks} dimensions={dimensions} />)
+        };*/
         return (
             <div>
                 <h2>{project.Name}</h2>
                 <hr />
                 <strong>For customer:</strong>
-                <span>{project.Customer.Name}</span>
+                <span>{!!project.Customer ? project.Customer.Name : "-"}</span>
                 <hr />
                 <h3>Tasks</h3>
                 {tasks}
