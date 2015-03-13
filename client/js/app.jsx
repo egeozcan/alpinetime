ffe24d81014b67a8a94ce4aecdf945b0d63a6caf
ege ozcan <egecan@gmail.com>
2015-03-14 00:53:41 +0100

    layout^^

--- client/js/app.jsx
+++ client/js/app.jsx
@@ -10,15 +10,38 @@ var RouteHandler = Router.RouteHandler;
 var ProjectListComponent = require("./components/project/ProjectList.jsx");
 var ProjectComponent = require("./components/project/Project.jsx");
 
+var Navbar = require('react-bootstrap/lib/Navbar');
+var Nav = require('react-bootstrap/lib/Nav');
+var MenuItem = require('react-bootstrap/lib/MenuItem');
+var DropdownButton = require('react-bootstrap/lib/DropdownButton');
+var Grid = require('react-bootstrap/lib/Grid');
+var Col = require('react-bootstrap/lib/Col');
+var ReactRouterBootstrap = require('react-router-bootstrap');
+var NavItemLink = ReactRouterBootstrap.NavItemLink;
+
 window.stateTree = require("./stateTree.js");
 var request = require('superagent');
 //Router.HistoryLocation.addChangeListener(console.log.bind(console))
+
+var Navigation = React.createClass({
+  render() {
+    return (
+      <Navbar fixedTop fluid inverse brand="Alpinetime">
+        <Nav>
+          <NavItemLink eventKey={1} to="projects">Projects</NavItemLink>
+          <NavItemLink eventKey={1} to="customers">Customers</NavItemLink>
+        </Nav>
+      </Navbar>
+    )
+  }
+})
 
 var App = React.createClass({
   render() {
     //var param = { projectID : 2 }
     return (
       <div>
+        <Navigation/>
         <RouteHandler/>
       </div>
     )
@@ -29,6 +52,7 @@ var App = React.createClass({
 var routes = (
   <Route name="app" path="/app" handler={App}>
     <Route name="projects" path="/app/projects" handler={ProjectListComponent}/>
+    <Route name="customers" path="/app/customers" handler={ProjectListComponent}/>
     <Route name="project" path="/app/project/:ID" handler={ProjectComponent}/>
   </Route>
 )
