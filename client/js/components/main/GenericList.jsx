494f79f0abe79e0a02372ca8c7f7950ee0c0e260
ege ozcan <egecan@gmail.com>
2015-03-16 23:23:17 +0100

    generic list implementation

--- client/js/components/main/GenericList.jsx
+++ client/js/components/main/GenericList.jsx
@@ -9,10 +9,51 @@ var Tree            = require("../../stateTree.js");
 var ProjectActions  = require("../../actions/projectActions.js");
 var StateActions    = require("../../actions/stateActions.js");
 
+var DefaultContainer = React.createClass({
+  let Titles = this.props.titles
+  render() {
+    return (
+      <Table>
+        <thead>
+          
+        </thead>
+      </Table>
+    );
+  }
+});
+
+var DefaultItem = React.createClass({
+  render() {
+    return <div></div>
+  }
+});
+
 var GenericList = React.createClass({
-    render() {
-        return (<p>Hello World</p>);
+  mixins: [Tree.mixin, Router.Navigation],
+  cursors: { stores: ['stores'], query: ['state', 'query'] },
+  propTypes:{
+    titles: React.PropTypes.arrayOf(
+      React.PropTypes.shape({
+        name: React.PropTypes.string,
+        titleElement: React.PropTypes.element
+      })
+    ),
+    itemElement: React.PropTypes.element,
+    containerElement: React.PropTypes.element
+  },
+  getDefaultProps() {
+    return {
+      itemElement: DefaultItem,
+      containerElement: DefaultContainer
     }
+  },
+  render() {
+    let Container = this.props.containerElement;
+    let Item = this.props.itemElement;
+    return (
+      
+    );
+  }
 });
 
 module.exports = GenericList;
