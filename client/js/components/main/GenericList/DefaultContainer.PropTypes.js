84938691e75cabb5a8d8d2019fb713fd60e16b84
Yavuz Ege Özcan <185294+egeozcan@users.noreply.github.com>
2015-03-19 00:35:23 +0100

    refactor genericlist

---
+++ client/js/components/main/GenericList/DefaultContainer.PropTypes.js
@@ -0,0 +1,15 @@
+var React = require('react');
+
+export default {
+  titles: React.PropTypes.arrayOf(
+    React.PropTypes.shape({
+      name: React.PropTypes.string,
+      getter: React.PropTypes.func,
+      title: React.PropTypes.oneOfType([
+        React.PropTypes.string,
+        React.PropTypes.element,
+      ])
+    }).isRequired
+  ),
+  data: React.PropTypes.array
+}
