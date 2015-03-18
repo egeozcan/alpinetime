dd30745649c2b8f818db401876a5836a527c4b90
ege ozcan <egecan@gmail.com>
2015-03-19 00:36:57 +0100

    statetree apparently isn't atomic, switch to local var thing

--- client/js/actions/stateActions.js
+++ client/js/actions/stateActions.js
@@ -3,16 +3,10 @@ var stateTree = require("../stateTree.js");
 var appState = stateTree.select("state");
 var is = require('is_js');
 
+var numInProgress = 0;
 function changeNumInProgress (level) {
-  appState.update({ numInProgress: { $apply(n) { return n + level } } });
-}
-function changePage (level) {
-  let cursor = appState.select(["query"]);
-  let page = parseInt((cursor.get().page || 1).toString());
-  if (page + level <= 0) {
-    return;
-  }
-  cursor.set("page", page + level);
+  numInProgress += level;
+  appState.set("numInProgress", numInProgress);
 }
 
 export default {
