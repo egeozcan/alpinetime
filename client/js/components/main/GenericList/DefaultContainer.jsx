f49d808a0a3e4a50b649877c119ca6e2a28d7789
Yavuz Ege Özcan <185294+egeozcan@users.noreply.github.com>
2015-03-23 22:37:47 +0100

    adds pre-calculated columns and purerender mixins in the mix

--- client/js/components/main/GenericList/DefaultContainer.jsx
+++ client/js/components/main/GenericList/DefaultContainer.jsx
@@ -1,13 +1,16 @@
 var ContainerPropTypes    = require('./GenericContainer.PropTypes.js');
 var React                 = require('react');
 var Table                 = require('react-bootstrap/lib/table');
+var PureRenderMixin       = require('react/addons').addons.PureRenderMixin;
 
 var DefaultContainer = React.createClass({
+  mixins: [PureRenderMixin],
   propTypes: ContainerPropTypes,
   render() {
+    var initialData = !!this.props.preCalculateForPage ? this.props.preCalculateForPage(this.props.data) : [];
     let Titles = this.props.titles.map(t => (<th key={t.name}>{t.title || t.name}</th>));
     let Rows = this.props.data.map((datarow, i) => {
-      let row = this.props.titles.map(t => (<td key={t.name}>{!!t.getter ? t.getter(datarow, i) : datarow[t.name]}</td>));
+      let row = this.props.titles.map(t => (<td key={t.name}>{!!t.getter ? t.getter(datarow, i, initialData) : datarow[t.name]}</td>));
       return (<tr key={i}>{row}</tr>);
     });
     return (
--- client/js/components/project/ProjectList.Titles.jsx
+++ client/js/components/project/ProjectList.Titles.jsx
@@ -1,6 +1,7 @@
 var React       = require('react');
 var Link = require('react-router').Link;
 var Customers   = require("../../stateTree.js").select(["stores", "customers"]);
+var Progressbar = require('react-bootstrap/lib/Progressbar');
 
 export default [
   { 
@@ -16,6 +17,12 @@ export default [
       let customer = Customers.get(c => c.ID === row.CustomerID) || {};
       let params = {ID: row.CustomerID};
       return (<Link to="customer" params={params}>{customer.Name || "Loading..."}</Link>)
+    }
+  },
+  {
+    name: "Progress",
+    getter() {
+      return (<Progressbar now={Math.random() * 100} label="%(percent)s%" />);
     }
   },
   { name: "Description" }
