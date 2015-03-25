545165cc14f92e3efec6835a1d0af2d626d3fe17
ege ozcan <egecan@gmail.com>
2015-03-25 01:11:16 +0100

    queryPrefix param to differentiate between multiple lists in the same page

--- client/js/components/main/GenericList/GenericList.jsx
+++ client/js/components/main/GenericList/GenericList.jsx
@@ -18,39 +18,50 @@ var GenericList = React.createClass({
       itemsInPage: 10
     }
   },
+  getQueryPrefix(prop) {
+    return (this.props.queryPrefix || "") + prop;
+  },
   getData() {
-    let data = this.cursors.stores.select(this.props.storeName).get();
+    let data = this.props.data || this.cursors.stores.select(this.props.storeName).get();
     if (!!this.props.filter) {
       return data.filter(this.props.filter);
     }
     return data;
   },
   getPageData() {
-    let data = this.getData().slice((this.page() - 1) * this.props.itemsInPage, this.page() * this.props.itemsInPage);
-    return data;
+    let allData = this.getData();
+    let pageData = allData.slice((this.page() - 1) * this.props.itemsInPage, this.page() * this.props.itemsInPage);
+    return { pageData, hasPaging: allData.length > this.props.itemsInPage };
   },
   page() {
-    let res = !this.cursors.query.get().page ? 1 : parseInt(this.cursors.query.get().page || 1);
+    let page = this.cursors.query.get()[this.getQueryPrefix("page")];
+    let res = !page ? 1 : parseInt(page || 1);
     return res;
   },
   incPage(e) {
-    if (this.hasNextPage()) {
-      this.transitionTo(location.pathname, null, {page: this.page() + 1});
-    }
     e.preventDefault();
+    if (this.hasNextPage()) {
+      let newQuery = Object.assign(this.cursors.query.get(), {[this.getQueryPrefix("page")]: this.page() + 1});
+      this.transitionTo(location.pathname, null, newQuery);
+    }
   },
   decPage(e) {
-    this.transitionTo(location.pathname, null, {page: Math.max(this.page() - 1, 1)});
     e.preventDefault();
+    var page = this.page() - 1;
+    if (page <= 0) {
+      return;
+    }
+    let newQuery = Object.assign(this.cursors.query.get(), {[this.getQueryPrefix("page")]: page})
+    this.transitionTo(location.pathname, null, newQuery);
   },
   hasNextPage() {
     return this.page() * this.props.itemsInPage < this.getData().length;
   },
   render() {
     let Container = this.props.containerElement;
-    let data = this.props.data || this.getPageData();
+    let data = this.getPageData();
     let pager = "";
-    if (data.length > 0 && (data.length > this.props.itemsInPage)) {
+    if (data.hasPaging) {
       pager = (
         <Pager>
           <PageItem onClick={this.decPage} disabled={this.page() === 1}>Previous</PageItem>
@@ -58,8 +69,8 @@ var GenericList = React.createClass({
         </Pager>)
     }
     return (
-      <Container data={data} titles={this.props.titles} preCalculateForPage={this.props.preCalculateForPage}>
-          
+      <Container data={data.pageData} titles={this.props.titles} hidetitles={this.props.hidetitles} preCalculateForPage={this.props.preCalculateForPage}>
+        {pager}
       </Container>
     );
   }
