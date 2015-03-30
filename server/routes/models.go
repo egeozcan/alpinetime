efb7509c8bddda7b3d958b408274853e0a36cb9b
ege ozcan <egecan@gmail.com>
2015-03-30 20:13:08 +0200

    get validation tags in models route

--- server/routes/models.go
+++ server/routes/models.go
@@ -2,9 +2,9 @@ package routes
 
 import (
 	"alpinetime/models"
-	"fmt"
 	"github.com/gin-gonic/gin"
 	"reflect"
+	"strings"
 )
 
 func getFields(model interface{}, res *map[string]interface{}) {
@@ -13,12 +13,16 @@ func getFields(model interface{}, res *map[string]interface{}) {
 	for i := 0; i < numFields; i++ {
 		field := mytype.FieldByIndex([]int{i})
 		if field.Anonymous {
-			if field.Name == "Record" {
-				getFields(&models.Record{}, res)
-			}
 			continue
 		}
-		(*res)[field.Name] = field
+		if idreffield, _ := mytype.FieldByName(field.Name + "ID"); idreffield.Name != "" {
+			continue
+		}
+		name := strings.Split(field.Tag.Get("json"), ",")[0]
+		if len(name) == 0 {
+			name = field.Name
+		}
+		(*res)[name] = strings.Split(field.Tag.Get("validation"), ",")
 	}
 }
 
@@ -31,7 +35,6 @@ func Models(c *gin.Context) {
 	}
 	for i := 0; i < len(structs); i++ {
 		name := reflect.TypeOf(structs[i]).Elem().Name()
-		fmt.Println(name)
 		tres := make(map[string]interface{})
 		getFields(structs[i], &tres)
 		res[name] = tres
