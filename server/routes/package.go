d9ee4c722d79f883fd467296771c856fc860dfab
Yavuz Ege Özcan <185294+egeozcan@users.noreply.github.com>
2015-04-21 17:07:57 +0200

    now comes with the ability to save packages

--- server/routes/package.go
+++ server/routes/package.go
@@ -3,7 +3,9 @@ package routes
 import (
 	"alpinetime/models"
 	"alpinetime/models/connection"
+	"errors"
 	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
 	"strconv"
 )
 
@@ -25,5 +27,13 @@ func Package(c *gin.Context) {
 }
 
 func AddPackage(c *gin.Context) {
-
+	var pkg models.Package
+	c.BindWith(&pkg, binding.JSON)
+	if pkg.Name == "" || pkg.ProjectID == 0 {
+		var err = errors.New("Package name and project ID are needed.")
+		c.Fail(500, err)
+		return
+	}
+	connection.Db.Save(&pkg)
+	c.JSON(200, pkg)
 }
