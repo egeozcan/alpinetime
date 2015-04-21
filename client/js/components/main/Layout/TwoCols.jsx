138b46f03db205fc13e82906e01ab42b4ad364ca
ege ozcan <egecan@gmail.com>
2015-04-22 00:42:42 +0200

    auto single-layout mode

--- client/js/components/main/Layout/TwoCols.jsx
+++ client/js/components/main/Layout/TwoCols.jsx
@@ -4,26 +4,35 @@ var Col = require('react-bootstrap/lib/Col');
 var Row = require('react-bootstrap/lib/Row');
 
 function ensureKeys(el) {
-	var index = 0;
 	if (!Array.isArray(el)) {
 		return el;
 	}
-	return el.map(_el => {
+	return el.map((_el, i) => {
 		if(!_el || !!_el.key) return _el;
-		if (!!_el._isReactElement) return React.cloneElement(_el, { key: index++ });
-		return (<div key={index++}>{_el}</div>);
+		if (!!_el._isReactElement) return React.cloneElement(_el, { key: i });
+		return (<div key={i}>{_el}</div>);
 	});
 }
 
 export default React.createClass({
 	render() {
+		let sidebar = null;
+		let mainColspan = 12;
+		let mainColPull = 0;
+		if (!!this.props.Sidebar && (!Array.isArray(this.props.Sidebar) || this.props.Sidebar.length > 0)) {
+			mainColspan = 8;
+			mainColPull = 4;
+			sidebar = (
+				<Col sm={4} smPush={8} className="content">
+					{ensureKeys(this.props.Sidebar)}
+				</Col>
+			)
+		}
 		return (
 			<Grid fluid role="main">
 				<Row>
-					<Col sm={4} smPush={8} className="content">
-						{ensureKeys(this.props.Sidebar)}
-					</Col>
-					<Col sm={8} smPull={4} className="content">
+					{sidebar}
+					<Col sm={mainColspan} smPull={mainColPull} className="content">
 						{ensureKeys(this.props.Content)}
 					</Col>
 				</Row>
