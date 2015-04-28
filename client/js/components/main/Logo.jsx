2afa947d9a2fabcf900c71ebc8366caf15445e70
Yavuz Ege Ozcan <egecan@gmail.com>
2015-04-28 10:12:37 +0200

    fixes lint errors: logo.jsx

--- client/js/components/main/Logo.jsx
+++ client/js/components/main/Logo.jsx
@@ -1,14 +1,16 @@
-var React = require('react');
+"use strict";
+
+var React = require("react");
 
 var logoStyles = {
-  maxHeight: 75,
-  minHeight: 50
-}
+    maxHeight: 75,
+    minHeight: 50
+};
 
 export default React.createClass({
-  render() {
-    return (
-      <img src="/public/logo.png" style={logoStyles} className="logo" alt="Alpinetime"/>
-    )
-  }
+    render() {
+        return (
+            <img src="/public/logo.png" style={logoStyles} className="logo" alt="Alpinetime"/>
+        );
+    }
 });
