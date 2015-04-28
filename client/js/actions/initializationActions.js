e3a1094e11e85d18fb03110a2c2347ecab139f05
Yavuz Ege Ozcan <egecan@gmail.com>
2015-04-28 10:11:18 +0200

    added a linter config and some fixes

--- client/js/actions/initializationActions.js
+++ client/js/actions/initializationActions.js
@@ -1,48 +1,58 @@
-var stateTree = require('../stateTree.js');
-var stateActions = require("./stateActions.js");
-var request = require("superagent");
-var URI = require('URIjs');
-var Router = require('react-router');
+"use strict";
 
-var loadEntity = (entityName) => {
-  stateActions.numInProgress.inc();
-  request.get("/api/" + entityName)
-    .end((err, res) => {
-      let store = stateTree.select("stores", entityName);
-      store.edit(JSON.parse(res.text));
-      stateActions.numInProgress.dec();
+let stateTree = require("../stateTree.js");
+let stateActions = require("./stateActions.js");
+let request = require("superagent");
+let URI = require("URIjs");
+let Router = require("react-router");
+
+let loadEntity = (entityName) => {
+    stateActions.numInProgress.inc();
+    request.get("/api/" + entityName)
+        .end((err, res) => {
+            if (err) {
+                console.log(`An error has occured: ${err}`);
+                return;
+            }
+            let store = stateTree.select("stores", entityName);
+            store.edit(JSON.parse(res.text));
+            stateActions.numInProgress.dec();
+        });
+};
+
+export let loadProjects = loadEntity.bind(null, "projects");
+export let loadCustomers = loadEntity.bind(null, "customers");
+export let loadTasks = loadEntity.bind(null, "tasks");
+export let loadPackages = loadEntity.bind(null, "packages");
+export let loadLookups = loadEntity.bind(null, "lookups");
+
+export let loadModelDefinitions = () => {
+    request.get("/api/definitions").end((err, res) => {
+        if (err) {
+            console.log(`An error has occured: ${err}`);
+            return;
+        }
+        stateTree.select("definitions").edit(JSON.parse(res.text));
     });
-}
-
-export var loadProjects = loadEntity.bind(null, "projects");
-export var loadCustomers = loadEntity.bind(null, "customers");
-export var loadTasks = loadEntity.bind(null, "tasks");
-export var loadPackages = loadEntity.bind(null, "packages");
-export var loadLookups = loadEntity.bind(null, "lookups");
-
-export var loadModelDefinitions = () => {
-  request.get('/api/definitions').end((err, res) => { 
-    stateTree.select("definitions").edit(JSON.parse(res.text));
-  });
-}
+};
 
 /* subscribe to the url change event and
-  update state when a significant change occurs */
+    update state when a significant change occurs */
 let lastUri = "";
 let sanitize = s => s.replace("?page=1", "");
-export var subscribeToQuery = () => {
-  let queryCursor = stateTree.select(["state", "query"]);
-  let updateQuery = (uri) => {
-    if (!!uri) {
-      if (sanitize(uri.path) == sanitize(lastUri)) { 
-        return;
-      }
-      lastUri = uri.path;
-    } else {
-      return;
-    }
-    queryCursor.edit(URI.parseQuery(location.search));
-  };
-  Router.HistoryLocation.addChangeListener(updateQuery);
-  updateQuery();
-}
+export let subscribeToQuery = () => {
+    let queryCursor = stateTree.select(["state", "query"]);
+    let updateQuery = (uri) => {
+        if (uri) {
+            if (sanitize(uri.path) === sanitize(lastUri)) {
+                return;
+            }
+            lastUri = uri.path;
+        } else {
+            return;
+        }
+        queryCursor.edit(URI.parseQuery(location.search));
+    };
+    Router.HistoryLocation.addChangeListener(updateQuery);
+    updateQuery();
+};
