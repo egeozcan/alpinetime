b0d839a0bcfe3c91b83f41b95a2c6bcecfbdc0b2
ege ozcan <egecan@gmail.com>
2015-04-29 00:53:15 +0200

    cleans actions

--- client/js/actions/customerActions.js
+++ client/js/actions/customerActions.js
@@ -1,21 +1,10 @@
-var request = require("superagent"); 
-var stateTree = require("../stateTree.js");
-var customerStore = stateTree.select("stores", "customers");
-var stateActions = require("./stateActions.js")
-
-
-function loadCustomer(id) {
-  id = id.toString();
-  return customerStore.select(p => p.ID === id);
-}
-
-const pageSize = 10;
+"use strict";
 
 export default {
-  add(customer) {
-  },
-  load(id) {
-  },
-  loadList(params) {
-  }
-}
+    add(/*customer*/) {
+    },
+    load(/*id*/) {
+    },
+    loadList(/*params*/) {
+    }
+};
