90e31cf5e1af264f6bb36a3534a339d4b815fb0a
Yavuz Ege Özcan <185294+egeozcan@users.noreply.github.com>
2015-04-29 10:20:13 +0200

    EstimatedMinutes & SpentMinutes should come from the Estimations and TimeEntries

--- models/task.go
+++ models/task.go
@@ -6,23 +6,21 @@ import (
 
 type Task struct {
 	Record
-	PackageID        int64  `json:",string"`
-	ProjectID        int64  `json:",string"`
-	Name             string `sql:"size:255"`
-	Description      string `sql:"size:2000"`
-	AssignedTo       *User
-	AssignedToID     int64 `json:",string"`
-	Estimations      []*Estimation
-	TaskStatus       *Lookup `json:"-"`
-	TaskStatusID     int64   `json:",string" validation:"ref(Lookup)"`
-	TaskCategory     *Lookup `json:"-"`
-	TaskCategoryID   int64   `json:",string" validation:"ref(Lookup)"`
-	TaskPriority     *Lookup `json:"-"`
-	TaskPriorityID   int64   `json:",string" validation:"ref(Lookup)"`
-	EstimatedMinutes int
-	SpentMinutes     int
-	StartedAt        time.Time
-	BlockedBy        *Task
-	BlockedByID      int64 `json:",string" form:"BlockedByID" validation:"ref(Task)"`
-	Steps            []*Step
+	PackageID      int64  `json:",string"`
+	ProjectID      int64  `json:",string"`
+	Name           string `sql:"size:255"`
+	Description    string `sql:"size:2000"`
+	AssignedTo     *User
+	AssignedToID   int64 `json:",string"`
+	Estimations    []*Estimation
+	TaskStatus     *Lookup `json:"-"`
+	TaskStatusID   int64   `json:",string" validation:"ref(Lookup)"`
+	TaskCategory   *Lookup `json:"-"`
+	TaskCategoryID int64   `json:",string" validation:"ref(Lookup)"`
+	TaskPriority   *Lookup `json:"-"`
+	TaskPriorityID int64   `json:",string" validation:"ref(Lookup)"`
+	StartedAt      time.Time
+	BlockedBy      *Task
+	BlockedByID    int64 `json:",string" form:"BlockedByID" validation:"ref(Task)"`
+	Steps          []*Step
 }
