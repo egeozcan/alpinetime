2abbd1d4eb41921c7f7f821267b380dd847b7f72
Yavuz Ege Ozcan <egecan@gmail.com>
2015-05-05 14:53:38 +0200

    add timeentry

--- models/user.go
+++ models/user.go
@@ -6,10 +6,11 @@ import (
 
 type User struct {
 	Record
-	Domain    string     `sql:"size:255" form:"domain" binding:"required"`
-	Name      string     `sql:"size:255" form:"username" binding:"required" validation:"minLength(3)"`
-	Email     string     `sql:"size:254" form:"email" validation:"custom(email)"`
-	Password  string     `sql:"-" form:"password" binding:"required" json:"-" validation:"password,minLength(3)"`
-	Projects  []*Project `gorm:"many2many:user_projects;"`
-	LastLogin time.Time  `json:"-"`
+	Domain      string     `sql:"size:255" form:"domain" binding:"required"`
+	Name        string     `sql:"size:255" form:"username" binding:"required" validation:"minLength(3)"`
+	Email       string     `sql:"size:254" form:"email" validation:"custom(email)"`
+	Password    string     `sql:"-" form:"password" binding:"required" json:"-" validation:"password,minLength(3)"`
+	Projects    []*Project `gorm:"many2many:user_projects;"`
+	TimeEntries []*TimeEntry
+	LastLogin   time.Time `json:"-"`
 }
