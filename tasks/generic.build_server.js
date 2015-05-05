eb7ea707cd189103041922485d472d418e3f7eb6
Yavuz Ege Ozcan <egecan@gmail.com>
2015-05-05 18:02:50 +0200

    adds a delayed tag to the build

--- tasks/generic.build_server.js
+++ tasks/generic.build_server.js
@@ -1,28 +1,30 @@
-var cp = require('child_process');
+"use strict";
 
-const debugCommands = ["go-bindata -debug -pkg data -o ./data/bindata.go ./public/...", "go build -tags 'debug'"];
+var cp = require("child_process");
+
+const debugCommands = ["go-bindata -debug -pkg data -o ./data/bindata.go ./public/...", "go build -tags \"debug delayed\""];
 const releaseCommands = ["go-bindata -pkg data -o ./data/bindata.go ./public/...", "go build -o alpinetime_release.exe"];
 
 module.exports = function(isDebug, autorun) {
-  var proc;
-  return function builder(callback) {
-    if(proc) {
-      proc.kill('SIGINT');
-    }
-    (isDebug ? debugCommands : releaseCommands).forEach(function (command) {
-      console.log("running: ", command);
-      cp.execSync(command, {cwd: process.cwd()});
-    });
-    if (autorun) {
-      proc = cp.spawn('./alpinetime');
-      proc.stdout.on('data', function (data) {
-        console.log(data.toString().replace(/\n+?$/m, ""));
-        if(!!callback && data.toString().indexOf('-- Started --') >= 0) {
-          callback();
+    var proc;
+    return function builder(callback) {
+        if(proc) {
+            proc.kill("SIGINT");
         }
-      });
-    } else if(!!callback) {
-      callback();
-    }
-  }
-}
+        (isDebug ? debugCommands : releaseCommands).forEach(function (command) {
+            console.log("running: ", command);
+            cp.execSync(command, {cwd: process.cwd()});
+        });
+        if (autorun) {
+            proc = cp.spawn("./alpinetime");
+            proc.stdout.on("data", function (data) {
+                console.log(data.toString().replace(/\n+?$/m, ""));
+                if(!!callback && data.toString().indexOf("-- Started --") >= 0) {
+                    callback();
+                }
+            });
+        } else if(callback) {
+            callback();
+        }
+    };
+};
