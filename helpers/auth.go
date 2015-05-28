c500e43eb04f82ee3d117ada50e48c48da01b0c0
Yavuz Ege Özcan <185294+egeozcan@users.noreply.github.com>
2015-05-28 11:02:50 +0200

    no-encrypt auth for debug mode

--- helpers/auth.go
+++ helpers/auth.go
@@ -5,16 +5,26 @@ package helpers
 import (
 	"fmt"
 	"github.com/go-ldap/ldap"
+	"alpinetime/models"
+	"alpinetime/models/connection"
 )
 
 var ldap_server string = "l-mobile.intern"
 var ldap_port uint16 = 389
 
-func Auth(username, password string) error {
+func Auth(username, password string) *models.User {
 	l, err := ldap.Dial("tcp", fmt.Sprintf("%s:%d", ldap_server, ldap_port))
 	if err != nil {
 		return err
 	}
 	defer l.Close()
-	return l.Bind(username, password)
+	if authErr := l.Bind(username, password); authErr == nil {
+		usr := &models.User{
+			Name: username,
+			Password: password,
+		}
+		connection.Db.Where(usr).First(usr)
+		return usr
+	}
+	return nil
 }
--- models/user.go
+++ models/user.go
@@ -9,7 +9,7 @@ type User struct {
 	Domain      string     `sql:"size:255" form:"domain" binding:"required"`
 	Name        string     `sql:"size:255" form:"username" binding:"required" validation:"minLength(3)"`
 	Email       string     `sql:"size:254" form:"email" validation:"custom(email)"`
-	Password    string     `sql:"-" form:"password" binding:"required" json:"-" validation:"password,minLength(3)"`
+	Password    string     `sql:"size:500" form:"password" binding:"required" json:"-" validation:"password,minLength(3)"`
 	Projects    []*Project `gorm:"many2many:user_projects;"`
 	TimeEntries []*TimeEntry
 	LastLogin   time.Time `json:"-"`
